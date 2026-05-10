a1ca76fdc28a6de542f4881e8c35de420ffa0f8d
revanthvelocius <revanth@velocius.ai>
2026-05-10 19:30:04 +0530

    Initial commit of Playwright batch 2x code

---
+++ Chapter01_Basics/03_JSCommands_verifysetup.js
@@ -0,0 +1,4 @@
+console.log(process.platform);
+//win32
+console.log(process.arch);
+//x64
---
+++ Chapter01_Basics/04_Hotcode.js
@@ -0,0 +1,11 @@
+console.log ("Hello");
+function add(a,b)
+{
+    return a + b;
+}
+let result;
+for (let i = 0; i<100000; i++)
+{
+    result = add(i, i + 1)
+}
+console.log("after 10000 calls:", result);
