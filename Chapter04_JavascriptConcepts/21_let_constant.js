2317020448a1907dc03ac7f58268f23aaa020e59
revanthvelocius <revanth@velocius.ai>
2026-05-18 23:46:10 +0530

    Save latest edits: Chapter04 and Chapter06 updates

--- Chapter04_JavascriptConcepts/21_let_constant.js
+++ Chapter04_JavascriptConcepts/21_let_constant.js
@@ -6,3 +6,23 @@ console.log("Hei");
 console.log("Hei");
 
 const c = "pramod;";
+
+console.log(a);
+var a = 10;
+
+console.log(a);
+let a = 10;
+
+console.log(a);
+const a = 10;
+
+
+greet();
+function greet() { console.log("Hi"); }
+
+
+Hi();
+const sayHi = function () { console.log("Hi"); };
+
+
+console.log(testName); let testName = "Login Test"; 
