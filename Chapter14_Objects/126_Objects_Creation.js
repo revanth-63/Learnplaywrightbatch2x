1efade70beb4c278797313622d543f28115c191b
revanthvelocius <revanth@velocius.ai>
2026-06-14 22:06:49 +0530

    Objects

---
+++ Chapter14_Objects/126_Objects_Creation.js
@@ -0,0 +1,17 @@
+let c = { status: "pass" };
+let d = { status: "pass" };
+console.log(c);
+console.log(d);
+
+if (c === d) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+if (b === a) {
+    console.log("true");
+} else {
+    console.log("false");
+}
